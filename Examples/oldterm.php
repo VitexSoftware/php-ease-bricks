efc366a3d956ed56a53a5912202bcb14126f4145
CyberVitexus <info@vitexsoftware.cz>
2023-11-14 19:15:29 +0100

    Multiple Terminals on page supported now

--- Examples/oldterm.php
+++ Examples/oldterm.php
@@ -17,6 +17,12 @@ $text = ob_get_clean();
 
 $oPage = new Ease\WebPage(_('Old Terminal'));
 
+$oPage->addItem(new \Ease\Html\H2Tag(_('Green Terminal')));
+
 $oPage->addItem(new Ease\ui\OldTerminal($text));
+
+$oPage->addItem(new \Ease\Html\H2Tag(_('Red Terminal')));
+
+$oPage->addItem((new Ease\ui\OldTerminal($text))->setBackRed(150)->setBackGreen(10)->setBlue(10)->setFontColor('yellow'));
 
 $oPage->draw();
--- src/Ease/ui/OldTerminal.php
+++ src/Ease/ui/OldTerminal.php
@@ -46,7 +46,11 @@ class OldTerminal extends \Ease\Html\DivTag
      */
     public function __construct($content = null, $properties = [])
     {
-        parent::__construct(new \Ease\Html\PreTag($content), $properties);
+        parent::__construct(null, $properties);
+        $this->addItem(new \Ease\Html\DivTag(new \Ease\Html\PreTag(new \Ease\Html\PairTag('output', [], $content), ['class' => 'console']), ['class' => 'faint','style' => 'z-index: 9']));
+        if (empty($this->getTagID())) {
+            $this->setTagID();
+        }
     }
 
     /**
@@ -54,40 +58,91 @@ class OldTerminal extends \Ease\Html\DivTag
      */
     public function finalize()
     {
+
         $this->addTagClass('OldTerminal');
         $this->addCSS('
 
-.OldTerminal {
+#' . $this->getTagID() . ' {
   background-color: black;
   background-image: radial-gradient(rgba(' . $this->red . ', ' . $this->green . ', ' . $this->blue . ', 0.75), black 120%);
-  height: 100vh;
   margin: 0;
+  padding: 0;
   overflow: hidden;
-  padding: 2rem;
-  color: white;
+  color: ' . $this->color . ';
   font: 1.3rem Inconsolata, monospace;
   text-shadow: 0 0 5px #C8C8C8;
 }
-.OldTerminal::after {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
+#' . $this->getTagID() . ' .faint {
+  margin: 0;
+  paddig: 0;
+  display: block;
+  height: 100%;
+  width: 100%;
   background: repeating-linear-gradient(0deg, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15) 1px, transparent 1px, transparent 2px);
   pointer-events: none;
 }
 
-.OldTerminal::selection {
+#' . $this->getTagID() . '::selection {
   background: #0080FF;
   text-shadow: none;
 }
 
-.OldTerminal pre {
+#' . $this->getTagID() . ' pre {
   margin: 0;
 }
 ');
         parent::finalize();
+    }
+
+    /**
+     * Set Background Red Color Value
+     *
+     * @param int $color 0-255
+     *
+     * @return self
+     */
+    public function setBackRed(int $color)
+    {
+        $this->red = $color;
+        return $this;
+    }
+
+    /**
+     * Set Background Green Color Value
+     *
+     * @param int $color 0-255
+     *
+     * @return self
+     */
+    public function setBackGreen(int $color)
+    {
+        $this->green = $color;
+        return $this;
+    }
+
+    /**
+     * Set Background Blue Color Value
+     *
+     * @param int $color 0-255
+     *
+     * @return self
+     */
+    public function setBlue(int $color)
+    {
+        $this->blue = $color;
+        return $this;
+    }
+
+    /**
+     * Set Font Color Value
+     *
+     * @param string $color color code or name
+     *
+     * @return self
+     */
+    public function setFontColor(string $color)
+    {
+        $this->color = $color;
+        return $this;
     }
 }
